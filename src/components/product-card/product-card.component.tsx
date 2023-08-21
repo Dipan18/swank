35d343bb0f512d206cab71259d07b78d43e000d1
Dipan <dipan188@gmail.com>
2023-08-21 18:07:53 -0400

    Refactored product-card component to typescript

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,22 +1,22 @@
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-
-import { selectCartItems } from '../../store/cart/cart.selector';
+import { useAppDispatch } from '../../hooks/redux-store.hooks';
 import { addItemToCart } from '../../store/cart/cart.reducer';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-
 import {
   ProductCardContainer,
   Footer,
   ProductName,
   ProductPrice,
 } from './product-card.styles';
+import { CartItem } from '../../store/cart/cart.type';
 
-const ProductCard = ({ product }) => {
-  const dispatch = useDispatch();
+type ProductCardProps = {
+  product: CartItem;
+};
 
-  const itemsInCart = useSelector(selectCartItems);
+const ProductCard = ({ product }: ProductCardProps) => {
+  const dispatch = useAppDispatch();
+
   const addProductToCart = () => dispatch(addItemToCart(product));
 
   const { name, imageUrl, price } = product;
@@ -28,7 +28,10 @@ const ProductCard = ({ product }) => {
         <ProductName>{name}</ProductName>
         <ProductPrice>${price}</ProductPrice>
       </Footer>
-      <Button onClick={addProductToCart} type={BUTTON_TYPE_CLASSES.inverted}>
+      <Button
+        onClick={addProductToCart}
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+      >
         Add To Cart
       </Button>
     </ProductCardContainer>
