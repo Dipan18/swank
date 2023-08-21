e2bfbe4f680d4b6ef588e1c2395b502d197fb132
Dipan <dipan188@gmail.com>
2023-08-21 18:36:27 -0400

    Refactored navigation component to typescript

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,25 +1,24 @@
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
-
+import { useAppSelector } from '../../hooks/redux-store.hooks';
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { selectIsCartOpen } from '../../store/cart/cart.selector';
 
 import { signOutLoggedInUser } from '../../network/firebase/firebase.auth';
 
 import { ReactComponent as NavLogo } from '../../assets/crown.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import {
   NavigationContainer,
   NavLinks,
   NavLink,
   LogoContainer,
-} from './navigation.styles.jsx';
+} from './navigation.styles';
 
 const Navigation = () => {
-  const currentUser = useSelector(selectCurrentUser);
-  const isCartOpen = useSelector(selectIsCartOpen);
+  const currentUser = useAppSelector(selectCurrentUser);
+  const isCartOpen = useAppSelector(selectIsCartOpen);
 
   return (
     <>
@@ -30,7 +29,7 @@ const Navigation = () => {
         <NavLinks>
           <NavLink to="/shop">Shop</NavLink>
           {currentUser ? (
-            <NavLink as="span" onClick={signOutLoggedInUser}>
+            <NavLink to="#" as="span" onClick={signOutLoggedInUser}>
               Sign Out
             </NavLink>
           ) : (
