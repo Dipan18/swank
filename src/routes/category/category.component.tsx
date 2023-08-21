d0f031da6ee3c7fb86c13fcf3487f2c9f391fe32
Dipan <dipan188@gmail.com>
2023-08-21 17:34:28 -0400

    Refactored category component to typescript

--- src/routes/category/category.component.tsx
+++ src/routes/category/category.component.tsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
+import { useAppSelector } from '../../hooks/redux-store.hooks';
 import { selectCategoriesMap } from '../../store/categories/category.selector';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
 import { CategoryContainer, CategoryTitle } from './category.styles';
 
+type CategoryRouteParams = {
+  category: string
+}
+
 const Category = () => {
-  const { category } = useParams();
-  const categoriesMap = useSelector(selectCategoriesMap);
+  const { category } = useParams<keyof CategoryRouteParams>() as CategoryRouteParams;
+  const categoriesMap = useAppSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
